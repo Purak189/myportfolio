b3b887dc72a1fc8f559a2a4983057aed4d051eb7
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-12-01 10:32:17 -0500

    feat: adding new project

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -15,6 +15,9 @@ import IconJWT from "@/assets/icons/technologiesIcons/IconJWT.vue";
 import IconSpringBoot from "@/assets/icons/technologiesIcons/IconSpringBoot.vue";
 import IconAngular from "@/assets/icons/technologiesIcons/IconAngular.vue";
 import IconPython from "@/assets/icons/technologiesIcons/IconPython.vue";
+import IconNodeJs from "@/assets/icons/technologiesIcons/IconNodeJs.vue";
+import IconExpress from "@/assets/icons/technologiesIcons/IconExpress.vue";
+import IconMongoDB from "@/assets/icons/technologiesIcons/IconMongoDB.vue";
 
 const imgProject1 = [
   '/Project1Carousel/img1.png',
@@ -39,6 +42,16 @@ const imgProject3 = [
   '/Project3Carousel/img3.jpeg'
 ];
 
+const imgProject4 = [
+  '/Project4Carousel/img1.jpg',
+  '/Project4Carousel/img2.jpg',
+  '/Project4Carousel/img3.jpg',
+  '/Project4Carousel/img4.jpg',
+  '/Project4Carousel/img5.jpg',
+  '/Project4Carousel/img6.jpg',
+  '/Project4Carousel/img7.jpg'
+];
+
 </script>
 
 <template>
@@ -52,50 +65,6 @@ const imgProject3 = [
       <IconLine></IconLine>
     </div>
     <div class="max-sm:w-full max-sm:px-2">
-      <div class="flex flex-col lg:flex-row items-center p-8 bg-background border border-brand-1 rounded-xl shadow-lg mx-auto my-6 w-3/4 max-lg:w-4/5 max-sm:px-4 max-sm:w-full">
-        <!-- Imagen del proyecto -->
-        <div class="w-full lg:w-2/5 p-4 max-sm:p-0">
-          <Carousel :autoplay="2000" :wrap-around="true">
-            <Slide v-for="(image, index) in imgProject1" :key="index">
-              <img :src="image" alt="Project Image" class="rounded-md "/>
-            </Slide>
-          </Carousel>
-        </div>
-
-        <!-- Información del proyecto -->
-        <div class="w-full lg:w-3/5 p-4 text-white flex flex-col gap-5 max-sm:p-2">
-          <!-- Título del proyecto -->
-          <div class="flex justify-between items-center">
-            <h2 class="text-2xl text-brand-1 leading-relaxed flex items-center underline">
-              Webmaster
-              <IconMousePointer class="w-5 mt-6 ml-2"></IconMousePointer>
-            </h2>
-            <!-- Enlaces a GitHub y Live Demo -->
-            <div class="flex space-x-4 mt-4 mr-3">
-              <a href="https://github.com/orgs/WebMastersUPC/repositories" target="_blank" class="text-teal-400 hover:text-teal-300">
-                <IconGitHub color="#FFFFFF" width="25" height="25" ></IconGitHub>
-              </a>
-              <a href="https://fromzeroupcapp.netlify.app" target="_blank" class="text-teal-400 hover:text-teal-300">
-                <IconLink color="#FFFFFF" width="25" height="25"></IconLink>
-              </a>
-            </div>
-          </div>
-
-          <!-- Descripción -->
-          <p class="mt-2 text-lg lg:text-base max-sm:text-sm text-justify">
-            Web Master es una plataforma web intuitiva y fácil de usar que conecta a las empresas con desarrolladores freelance. Esta aplicación permite a las empresas publicar sus proyectos de software, mientras que los desarrolladores pueden postularse para colaborar en ellos. Construida con tecnologías como Vue, PrimeVue, Tailwind, .NET Core y MySQL.
-          </p>
-
-          <!-- Tecnologías usadas -->
-          <div class="flex flex-wrap mt-4 space-x-2 max-sm:space-x-1">
-            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center"><IconVue></IconVue> Vue.js</span>
-            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center"><IconTailwind></IconTailwind> Tailwind</span>
-            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center"><IconMySql></IconMySql> MySQL</span>
-            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-sm:my-2"><IconNetCore></IconNetCore> .NET Core</span>
-            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-lg:my-2"><IconJWT></IconJWT> JSON Web Token</span>
-          </div>
-        </div>
-      </div>
       <div class="flex flex-col lg:flex-row items-center p-8 bg-background border border-brand-1 rounded-xl shadow-lg mx-auto my-6 w-3/4 max-lg:w-4/5 max-sm:px-4 max-sm:w-full">
         <!-- Imagen del proyecto -->
         <div class="w-full lg:w-2/5 p-4 max-sm:p-0">
@@ -136,6 +105,93 @@ const imgProject3 = [
             <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center"><IconTailwind></IconTailwind> Tailwind</span>
             <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-sm:my-2"><IconMySql></IconMySql> MySQL</span>
             <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-lg:my-2"><IconSpringBoot></IconSpringBoot> Spring Boot</span>
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-lg:my-2"><IconJWT></IconJWT> JSON Web Token</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col lg:flex-row items-center p-8 bg-background border border-brand-1 rounded-xl shadow-lg mx-auto my-6 w-3/4 max-lg:w-4/5 max-sm:px-4 max-sm:w-full">
+        <!-- Imagen del proyecto -->
+        <div class="w-full lg:w-2/5 p-4 max-sm:p-0">
+          <Carousel :autoplay="2000" :wrap-around="true">
+            <Slide v-for="(image, index) in imgProject4" :key="index">
+              <img :src="image" alt="Project Image" class="rounded-md" />
+            </Slide>
+          </Carousel>
+        </div>
+
+        <!-- Información del proyecto -->
+        <div class="w-full lg:w-3/5 p-4 text-white flex flex-col gap-5 max-sm:p-2">
+          <!-- Título del proyecto -->
+          <div class="flex justify-between items-center">
+            <h2 class="text-2xl text-brand-1 leading-relaxed flex items-center underline">
+              CreciTurismo
+              <IconMousePointer class="w-5 mt-6 ml-2"></IconMousePointer>
+            </h2>
+            <!-- Enlaces a GitHub y Live Demo -->
+            <div class="flex space-x-4 mt-4 mr-3">
+              <a href="https://github.com/orgs/AyniHackaton2024-TheBugBusters/repositories" target="_blank" class="text-teal-400 hover:text-teal-300">
+                <IconGitHub color="#FFFFFF" width="25" height="25"></IconGitHub>
+              </a>
+            </div>
+          </div>
+
+          <!-- Descripción -->
+          <p class="mt-2 text-lg lg:text-base max-sm:text-sm text-justify">
+            CreciTurismo es una plataforma desarrollada en una hackathon de 2 días, diseñada para facilitar la formalización de empresas de turismo en el Perú. El sistema permite a las micro y pequeñas empresas (MYPES) completar formularios digitales que funcionan como declaraciones juradas, agilizando los trámites de formalización.
+
+            Una vez formalizadas, las empresas pueden registrar sus servicios turísticos a través de formularios adicionales. Estos datos se integran directamente en la base de datos del MINCETUR, permitiendo que sus servicios sean visibles para turistas nacionales y extranjeros. Este enfoque busca fortalecer el crecimiento de las MYPES del sector turístico y promover su inclusión en el mercado formal.
+          </p>
+
+          <!-- Tecnologías usadas -->
+          <div class="flex flex-wrap mt-4 space-x-2 max-sm:space-x-1">
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center"><IconAngular></IconAngular> Angular</span>
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center"><IconTailwind></IconTailwind> Tailwind</span>
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-sm:my-2"><IconNodeJs></IconNodeJs> Node.js</span>
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-lg:my-2"><IconExpress></IconExpress> Express</span>
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-lg:my-2"><IconMongoDB></IconMongoDB> MongoDB</span>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col lg:flex-row items-center p-8 bg-background border border-brand-1 rounded-xl shadow-lg mx-auto my-6 w-3/4 max-lg:w-4/5 max-sm:px-4 max-sm:w-full">
+        <!-- Imagen del proyecto -->
+        <div class="w-full lg:w-2/5 p-4 max-sm:p-0">
+          <Carousel :autoplay="2000" :wrap-around="true">
+            <Slide v-for="(image, index) in imgProject1" :key="index">
+              <img :src="image" alt="Project Image" class="rounded-md "/>
+            </Slide>
+          </Carousel>
+        </div>
+
+        <!-- Información del proyecto -->
+        <div class="w-full lg:w-3/5 p-4 text-white flex flex-col gap-5 max-sm:p-2">
+          <!-- Título del proyecto -->
+          <div class="flex justify-between items-center">
+            <h2 class="text-2xl text-brand-1 leading-relaxed flex items-center underline">
+              Webmaster
+              <IconMousePointer class="w-5 mt-6 ml-2"></IconMousePointer>
+            </h2>
+            <!-- Enlaces a GitHub y Live Demo -->
+            <div class="flex space-x-4 mt-4 mr-3">
+              <a href="https://github.com/orgs/WebMastersUPC/repositories" target="_blank" class="text-teal-400 hover:text-teal-300">
+                <IconGitHub color="#FFFFFF" width="25" height="25" ></IconGitHub>
+              </a>
+              <a href="https://fromzeroupcapp.netlify.app" target="_blank" class="text-teal-400 hover:text-teal-300">
+                <IconLink color="#FFFFFF" width="25" height="25"></IconLink>
+              </a>
+            </div>
+          </div>
+
+          <!-- Descripción -->
+          <p class="mt-2 text-lg lg:text-base max-sm:text-sm text-justify">
+            Web Master es una plataforma web intuitiva y fácil de usar que conecta a las empresas con desarrolladores freelance. Esta aplicación permite a las empresas publicar sus proyectos de software, mientras que los desarrolladores pueden postularse para colaborar en ellos. Construida con tecnologías como Vue, PrimeVue, Tailwind, .NET Core y MySQL.
+          </p>
+
+          <!-- Tecnologías usadas -->
+          <div class="flex flex-wrap mt-4 space-x-2 max-sm:space-x-1">
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center"><IconVue></IconVue> Vue.js</span>
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center"><IconTailwind></IconTailwind> Tailwind</span>
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center"><IconMySql></IconMySql> MySQL</span>
+            <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-sm:my-2"><IconNetCore></IconNetCore> .NET Core</span>
             <span class="px-3 py-1 bg-transparent border-brand-1 border-2 text-sm rounded-full flex gap-3 items-center max-lg:my-2"><IconJWT></IconJWT> JSON Web Token</span>
           </div>
         </div>
